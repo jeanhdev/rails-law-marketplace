bfdfa795fb1b44dbfa3521e493056c6fdff86505
Léopold Wolfrom <leopold.wolfrom@essec.edu>
2019-08-19 22:15:28 +0200

    Model + Controller basique de Services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,40 @@
 class ServicesController < ApplicationController
+  def index
+    @services = Service.all
+  end
+
+  def show
+    @service = Service.find(params[:id])
+  end
+
+  def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+    if @service.save
+      redirect_to service_path(@service)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @service = Service.find(params[:id])
+  end
+
+  def update
+    @service = Service.find(params[:id])
+    if @service.update(service_params)
+      redirect_to service_path(@service)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def service_params
+    params.require(:service).permit(:title, :description, :duration, :category, :price)
+  end
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,10 @@
 class Service < ApplicationRecord
+  CATEGORIES = ["Divorce", "Pension Alimentaire", "Stationnement", "Excès de vitesse", "Licenciement", "Harcèlement"]
   belongs_to :user
+  has_many :procedures
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :duration, presence: true
+  validates :price, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
