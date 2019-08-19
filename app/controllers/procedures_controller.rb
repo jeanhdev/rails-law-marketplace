135bc66cd7796114a5ac8a73504170034f969ea5
Philippe Boyer <bome.contact@gmail.com>
2019-08-19 20:03:30 +0200

    setup mvc procedure in progress

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -1,2 +1,33 @@
 class ProceduresController < ApplicationController
+  before_action :current_procedure, only: [:show, :edit, :update]
+  def index
+    @procedures = Procedure.all
+  end
+
+  def show
+  end
+
+  def new
+    @procedure = Procedure.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def current_procedure
+    @procedure = Procedure.find(params:id)
+  end
+
+  def params_procedure
+    params.require(:procedure).permit(:date, :status, :grievance)
+  end
+
 end
