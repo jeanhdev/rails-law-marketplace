3b3f0e567fbb5e48e41aa7185314a8c77068cd87
Dino Makhloufi <dino.makhloufi@gmail.com>
2019-08-20 00:36:39 +0200

    modif model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   validates :avatar, presence: true
 
   # verifier que cette ligne pour l'avatar est utile
-  has_attachment :avatar
+  # has_attachment :avatar
 
   JOBS = ['lawyer', 'notary', 'bailiff', 'jurist', 'justiciable']
   validates :job, inclusion: { in: JOBS }
