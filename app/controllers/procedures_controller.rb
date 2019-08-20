94b8b3707dbc39693a9ec4aa7d45efe2267c2586
Philippe Boyer <bome.contact@gmail.com>
2019-08-20 16:18:58 +0200

    procedure connected to User & Service

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -8,19 +8,20 @@ class ProceduresController < ApplicationController
   end
 
   def new
+    @service = Service.find(params[:service_id])
     @procedure = Procedure.new
   end
 
   def create
+    @service = Service.find(params[:service_id])
     @procedure = Procedure.new(procedure_params)
-    @procedure.save
-    redirect_to procedures_path
-  end
-
-  def edit
-  end
-
-  def update
+    @procedure.service = @service
+    @procedure.user = current_user
+    if @procedure.save
+      redirect_to service_path(@service), notice: 'Votre prestation a bien été créé.'
+    else
+      render 'new'
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get 'profile', to: 'dashboard#profile'
   root to: 'pages#home'
 
-  resources :users, only: [:show, :edit, :update, :delete] do
-    resources :services
-    resources :procedures, only: [:new, :create]
+  resources :users, only: [:show, :edit, :update, :delete]
+  resources :services do
+    resources :procedures, except: [:destroy]
   end
 
   get '/edit_lawyer', to: 'users#edit_lawyer', as: 'edit_lawyer'
