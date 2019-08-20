275bbd6aa81db5d602cba561af0302eaaf27a43c
Léopold Wolfrom <leopold.wolfrom@essec.edu>
2019-08-20 11:07:38 +0200

    Model + Controller + View (basique) de Services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,10 +1,11 @@
 class ServicesController < ApplicationController
+  before_action :set_service, only: [:show, :edit, :update, :destroy]
+
   def index
     @services = Service.all
   end
 
   def show
-    @service = Service.find(params[:id])
   end
 
   def new
@@ -14,26 +15,34 @@ class ServicesController < ApplicationController
   def create
     @service = Service.new(service_params)
     if @service.save
-      redirect_to service_path(@service)
+      redirect_to service_path(@service), notice: 'Votre prestation a bien été créé.'
     else
       render 'new'
     end
   end
 
   def edit
-    @service = Service.find(params[:id])
   end
 
   def update
-    @service = Service.find(params[:id])
     if @service.update(service_params)
-      redirect_to service_path(@service)
+      redirect_to service_path(@service), notice: 'Votre prestation a bien été modifié.'
     else
       render 'edit'
     end
   end
 
+  def destroy
+    @service.destroy
+    redirect_to services_path, notice: 'Votre prestation a bien été supprimé.'
+  end
+
   private
+
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
   def service_params
     params.require(:service).permit(:title, :description, :duration, :category, :price)
   end
