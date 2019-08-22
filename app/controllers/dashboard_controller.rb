693336d363f68b9e8121e536e3afe34519971a5a
Léopold Wolfrom <leopold.wolfrom@essec.edu>
2019-08-22 17:34:55 +0200

    new user dashboard for procedures

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
 
   def profile
     @user = current_user
+    @jobs = @user.job
     @services = @user.services
     @procedures = @user.procedures
   end
