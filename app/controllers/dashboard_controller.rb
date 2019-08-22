eff6b0ad4d468f92efb6b3e01b59577def4cd600
Léopold Wolfrom <leopold.wolfrom@essec.edu>
2019-08-22 15:32:10 +0200

    jjob = nil => no more justiciable

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
 
   def profile
     @user = current_user
-    @services = @user.services if @user.job != 'justiciable'
+    @services = @user.services if @user.job.nil?
     @procedures = @user.procedures
   end
 end
