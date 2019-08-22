796db70723259e1093b699a7da533e64ab177697
Léopold Wolfrom <leopold.wolfrom@essec.edu>
2019-08-22 15:09:47 +0200

    booking procedure + modal + accept procedure in dashboard

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -18,20 +18,34 @@ class ProceduresController < ApplicationController
     @procedure.service = @service
     @procedure.user = current_user
     if @procedure.save
-      redirect_to service_path(@service), notice: 'Votre prestation a bien été créé.'
+      # redirect_to service_path(@service), notice: 'Votre prestation a bien été créé.'
+      redirect_to profile_path, notice: 'Votre prestation a bien été créé.'
     else
       render 'new'
     end
   end
 
+  def accept
+    @procedure = Procedure.find(params[:id])
+    @procedure.status = "accepted"
+    @procedure.save
+    redirect_to profile_path, notice: "Vous venez d'accepter une procédure."
+  end
+
+  def refuse
+    @procedure = Procedure.find(params[:id])
+    @procedure.status = "refused"
+    @procedure.save
+    redirect_to profile_path, notice: "Vous venez de refuser une procédure."
+  end
+
   private
 
   def current_procedure
-    @procedure = Procedure.find(params:id)
+    @procedure = Procedure.find(params[:id])
   end
 
   def procedure_params
     params.require(:procedure).permit(:date, :grievance)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'profile', to: 'dashboard#profile'
   root to: 'pages#home'
+  patch 'procedures/:id/accept', to: 'procedures#accept', as: "accept"
+  patch 'procedures/:id/refuse', to: 'procedures#refuse', as: "refuse"
 
   resources :users, only: [:edit, :update]
   resources :services do
